dcaf7fbcc0f814e76995f9a4eae235df70f94dd5
manpreet singh <manpreet_jsb@hotmail.com>
2022-03-22 00:25:15 +0100

    added formik and axios, catching errors and fixing search results

--- src/Components/Search/search.tsx
+++ src/Components/Search/search.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import Paper from '@mui/material/Paper'
-import InputBase from '@mui/material/InputBase'
+
 import IconButton from '@mui/material/IconButton'
 import SearchIcon from '@mui/icons-material/Search'
 import CircularProgress from '@mui/material/CircularProgress'
@@ -11,100 +11,175 @@ import TableCell from '@mui/material/TableCell'
 import TableContainer from '@mui/material/TableContainer'
 import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
+import { Formik, Form, ErrorMessage, Field } from 'formik'
+import Alert from '@mui/material/Alert'
+import { TextField } from 'formik-material-ui'
+import Typography from '@mui/material/Typography'
+import axios from 'axios'
+import { IResult } from './data.type'
 
-//import axios from 'axios'
+interface Values {
+  search: string
+}
+
+const init: any = []
 
 const Search: React.FC = () => {
-  const [results, setResults] = useState<any>([])
-  const [isLoading, setIsLoading] = useState<Boolean | null>(null)
+  const [results, setResults] = useState<IResult[]>(init)
+  const [isSubmitting, setIsSubmitting] = useState<Boolean>(false)
+  const [apiError, setApiError] = useState<String | null>(null)
+
   const apiUrl = process.env.REACT_APP_API_URL
     ? process.env.REACT_APP_API_URL
     : ''
 
-  const getApiData = async () => {
-    setIsLoading(true)
-    const response = await fetch(apiUrl).then((response) => response.json())
-
-    setResults(response.docs)
-    setIsLoading(false)
-    console.log(results)
+  const getApiData = async (values: string) => {
+    setResults(init)
+    await axios
+      .get(apiUrl + values)
+      .then((response) => {
+        if (!response.data.docs.length) {
+          setApiError('No Records Found, Please search different Keyword.')
+          setIsSubmitting(false)
+        } else {
+          setResults(response.data.docs)
+          setIsSubmitting(false)
+          setApiError(null)
+        }
+      })
+      .catch((error) => {
+        setApiError(error)
+      })
   }
 
   return (
     <Box width={1}>
-      <Paper
-        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+      <Formik
+        initialValues={{
+          search: '',
+        }}
+        validate={(values) => {
+          const errors: Partial<Values> = {}
+          if (!values.search.trim()) {
+            errors.search = 'Please write Search Keyword'
+          }
+          return errors
+        }}
+        onSubmit={(values) => {
+          setIsSubmitting(true)
+          setApiError(null)
+          getApiData(values.search)
+        }}
       >
-        <InputBase
-          sx={{ ml: 1, flex: 1 }}
-          placeholder='Lord of the rings'
-          inputProps={{ 'aria-label': 'Lord of the rings' }}
-          value='Lord of the rings'
-        />
-        <IconButton
-          type='submit'
-          sx={{ p: '10px' }}
-          aria-label='search'
-          onClick={getApiData}
-        >
-          <SearchIcon />
-          {isLoading && <CircularProgress />}
-        </IconButton>
-      </Paper>
-      <Box my={5}>
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label='simple table'>
-            <TableHead>
-              <TableRow>
-                <TableCell align='right' size='small' width={150}>
-                  Cover
-                </TableCell>
-                <TableCell align='right' size='small' width={150}>
-                  Title
-                </TableCell>
-                <TableCell align='right' size='small' width={150}>
-                  Author
-                </TableCell>
-                <TableCell
-                  align='right'
+        {({ submitForm }) => (
+          <>
+            <Form>
+              <Paper
+                sx={{
+                  p: '2px 4px',
+                  display: 'flex',
+                  alignItems: 'center',
+                }}
+              >
+                <Field
+                  component={TextField}
+                  sx={{ ml: 1, flex: 1, borderColor: '0' }}
+                  placeholder='Please Write Book Title'
+                  name='search'
+                  inputProps={{
+                    'aria-label': 'Without label',
+                    name: 'search',
+                  }}
+                  disabled={isSubmitting}
                   size='small'
-                  width={150}
-                  sx={{ overflow: 'hidden' }}
+                />
+                <IconButton
+                  type='submit'
+                  sx={{ p: '10px' }}
+                  aria-label='search'
+                  onClick={submitForm}
                 >
-                  Year
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {results.map((row: any) => (
-                <TableRow
-                  key={row.Title}
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-                  <TableCell align='left'>
-                    {row.cover_i ? (
-                      <img
-                        src={`https://covers.openlibrary.org/b/id/${row.cover_i}-S.jpg`}
-                      />
-                    ) : (
-                      'Image not Available'
-                    )}
+                  {isSubmitting ? (
+                    <CircularProgress color='inherit' size={28} />
+                  ) : (
+                    <SearchIcon />
+                  )}
+                </IconButton>
+              </Paper>
+            </Form>
+            <ErrorMessage
+              name='search'
+              render={(msg) => <Alert severity='error'>{msg}</Alert>}
+            />
+          </>
+        )}
+      </Formik>
+      <Box my={5}>
+        {apiError ? (
+          apiError
+        ) : (
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+              <TableHead>
+                <TableRow>
+                  <TableCell align='right' size='small' width={150}>
+                    <Typography component='h5' variant='h5'>
+                      Cover
+                    </Typography>
                   </TableCell>
-                  <TableCell align='right'>{row.title}</TableCell>
-                  <TableCell align='right'>{row.author_name}</TableCell>
+                  <TableCell align='right' size='small' width={150}>
+                    <Typography component='h5' variant='h5'>
+                      Title
+                    </Typography>
+                  </TableCell>
+                  <TableCell align='right' size='small' width={150}>
+                    <Typography component='h5' variant='h5'>
+                      Author
+                    </Typography>
+                  </TableCell>
                   <TableCell
-                    align='right'
+                    align='left'
                     size='small'
                     width={150}
                     sx={{ overflow: 'hidden' }}
                   >
-                    {row.publish_year.join(',')}
+                    <Typography component='h5' variant='h5'>
+                      Year
+                    </Typography>
                   </TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+              </TableHead>
+
+              <TableBody>
+                {results.map((row: any) => (
+                  <TableRow
+                    key={row.Title}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell align='left'>
+                      {row.cover_i ? (
+                        <img
+                          src={`https://covers.openlibrary.org/b/id/${row.cover_i}-S.jpg`}
+                        />
+                      ) : (
+                        'Image not Available'
+                      )}
+                    </TableCell>
+                    <TableCell align='right'>{row.title}</TableCell>
+                    <TableCell align='right'>{row.author_name}</TableCell>
+                    <TableCell
+                      align='left'
+                      size='small'
+                      sx={{ overflow: 'hidden' }}
+                    >
+                      {row.publish_year.join(',')}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        )}
       </Box>
     </Box>
   )
